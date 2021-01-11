4f0dc69ac0369fc1d534b5f4a0a02f001c0f4e2a
hellowch <525691465@qq.com>
2021-01-11 21:36:47 +0800

    0.0.7 完成第六次作业第一题

--- app/src/main/java/com/example/myapplication/work20201225/Q_6_1.java
+++ app/src/main/java/com/example/myapplication/work20201225/Q_6_1.java
@@ -4,13 +4,83 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
 import com.example.myapplication.R;
 
 public class Q_6_1 extends AppCompatActivity {
+
+    // 百度地图组件
+    private MapView mMapView = null;
+    // 百度地图对象
+    private BaiduMap mBaiduMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q_6_1);
+
+        init();	//调用init()方法
+    }
+
+    private void init() {
+        mMapView = (MapView) findViewById(R.id.bmapView); //获取地图组件
+        mBaiduMap=mMapView.getMap();	//获取百度地图对象
+        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);		//普通地图
+        //设定中心点坐标
+        LatLng cenpt = new LatLng(30.319142, 120.352540);
+        //定义地图状态
+        MapStatus mMapStatus = new MapStatus.Builder().target(cenpt).zoom(15).build();
+        //定义MapStatusUpdate对象，以便描述地图状态将要发生的变化
+        MapStatusUpdate mMapStatusUpdate = MapStatusUpdateFactory.newMapStatus(mMapStatus);
+        //改变地图状态
+        mBaiduMap.setMapStatus(mMapStatusUpdate);
+
+        // 定义Maker坐标点
+        LatLng point = new LatLng(30.319142, 120.352540);
+        // 构建Marker图标
+        BitmapDescriptor bitmap = BitmapDescriptorFactory
+                .fromResource(R.drawable.ico);
+        // 构建MarkerOption，用于在地图上添加Marker
+        OverlayOptions option = new MarkerOptions().position(point)
+                .icon(bitmap);
+        // 在地图上添加Marker，并显示
+        mBaiduMap.addOverlay(option);
+
+
+//        mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);	//卫星地图
+        //开启交通图
+//        mBaiduMap.setTrafficEnabled(true);
+    }
+
+    //重开
+    @Override
+    protected void onResume(){
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    //暂停
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    //销毁
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+        mMapView = null;
     }
 }
